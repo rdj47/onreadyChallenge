a41008876c49a2b67bf5c02cbe12efee2165db57
Richard Villamizar <rdj47@hotmail.com>
2020-12-29 00:04:57 -0500

    New: Information required is delivered through class methods instead of handling intances.

--- scripts/script.js
+++ scripts/script.js
@@ -33,9 +33,41 @@ class Inventory {
         this.items = inventory;
     }  
 
+    allItems() {
+        this.items.forEach(element => {
+            if (element instanceof Vehicle) { 
+                console.log (''.concat('Marca: ', element.brand,' // ','Modelo: ', element.model, ' // ', 'Puertas: ', element.doors, ' // ', 'Precio: ', '$'.concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(element.price))));
+                //console.log ("Marca: ", element.brand," // ","Modelo: ", element.model, " // ", "Puertas: ", element.doors, " // ", "Precio: ", "$".concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(element.price)));
+            } else if (element instanceof Motorcycle) {
+                console.log (''.concat('Marca: ', element.brand,' // ','Modelo: ', element.model, ' // ', 'Cilindrada: ', element.cc, 'cc', ' // ', 'Precio: ', '$'.concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(element.price))));
+            }
+        });
+    }
+
     sortDescByPrice() {
-        this.items.sort((a, b) =>  b.price - a.price );
+        let itemsCopy = this.items;
+        itemsCopy.sort((a, b) =>  b.price - a.price );
+        console.log('Vehículos ordenados por precio de mayor a menor:');
+        itemsCopy.forEach(element => {
+            console.log (''.concat(element.brand,' ',element.model));
+        });
     }    
+
+    mostExpItem() {
+        let itemsCopy = this.items;
+        itemsCopy.sort((a, b) =>  b.price - a.price );
+        console.log(''.concat('Vehículo más caro: ', itemsCopy[0].brand, ' ', itemsCopy[0].model));
+    }
+
+    mostChpItem() {
+        let itemsCopy = this.items;
+        itemsCopy.sort((a, b) =>  b.price - a.price );
+        console.log(''.concat('Vehículo más barato: ', inventory.items[inventory.items.length-1].brand, ' ', inventory.items[inventory.items.length-1].model));
+    }
+    yItem() {
+        let yItem = this.items.find(element => element.brand.includes('Y') === true);
+        console.log(''.concat('Vehículo que contiene en el modelo la letra ‘Y’: ', yItem.brand, ' ', yItem.model, ' ', '$'.concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(yItem.price))));
+    }
   
 }
 
@@ -51,30 +83,36 @@ inventoryArray.push(vehicle2);
 inventoryArray.push(motorcycle2);
 //console.log ("Inventory Array: ", inventoryArray);
 
+
 let inventory = new Inventory(inventoryArray);
+inventory.allItems();
 //console.log ("Inventory: ", inventory.items);
-inventory.items.forEach(element => {
+/*inventory.items.forEach(element => {
     if (element instanceof Vehicle) { 
         console.log (''.concat('Marca: ', element.brand,' // ','Modelo: ', element.model, ' // ', 'Puertas: ', element.doors, ' // ', 'Precio: ', '$'.concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(element.price))));
         //console.log ("Marca: ", element.brand," // ","Modelo: ", element.model, " // ", "Puertas: ", element.doors, " // ", "Precio: ", "$".concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(element.price)));
     } else if (element instanceof Motorcycle) {
         console.log (''.concat('Marca: ', element.brand,' // ','Modelo: ', element.model, ' // ', 'Cilindrada: ', element.cc, 'cc', ' // ', 'Precio: ', '$'.concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(element.price))));
     }
-});
-inventory.sortDescByPrice();
+});*/
+//inventory.sortDescByPrice();
 //console.log ("Inventory (ordered descendig by price): ", inventory.items);
 
 console.log('=============================');
-console.log(''.concat('Vehículo más caro: ', inventory.items[0].brand, ' ', inventory.items[0].model));
-console.log(''.concat('Vehículo más barato: ', inventory.items[inventory.items.length-1].brand, ' ', inventory.items[inventory.items.length-1].model));
-let yItem = inventory.items.find(element => element.brand.includes('Y') === true);
-console.log(''.concat('Vehículo que contiene en el modelo la letra ‘Y’: ', yItem.brand, ' ', yItem.model, ' ', '$'.concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(yItem.price))));
+inventory.mostExpItem()
+//console.log(''.concat('Vehículo más caro: ', inventory.items[0].brand, ' ', inventory.items[0].model));
+inventory.mostChpItem();
+//console.log(''.concat('Vehículo más barato: ', inventory.items[inventory.items.length-1].brand, ' ', inventory.items[inventory.items.length-1].model));
+inventory.yItem();
+//let yItem = inventory.items.find(element => element.brand.includes('Y') === true);
+//console.log(''.concat('Vehículo que contiene en el modelo la letra ‘Y’: ', yItem.brand, ' ', yItem.model, ' ', '$'.concat(Intl.NumberFormat("de-DE", {minimumFractionDigits: 2}).format(yItem.price))));
 
 console.log('=============================');
-console.log('Vehículos ordenados por precio de mayor a menor:');
+inventory.sortDescByPrice();
+/*console.log('Vehículos ordenados por precio de mayor a menor:');
 inventory.items.forEach(element => {
     console.log (''.concat(element.brand,' ',element.model));
-});
+});*/
 
 
 /*inventory.items.forEach(element => {
